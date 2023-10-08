858739ddfc1f1597639134dc5a2acc3a5f99cf22
makuochi <makuochiokoye@gmail.com>
2023-10-08 06:14:24 -0700

    udate2

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['54.86.220.207', '54.175.137.217']
+env.hosts = ['54.90.3.40', '54.237.120.65']
 
 
 @runs_once
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['54.86.220.207', '54.175.137.217']
+env.hosts = ['54.90.3.40', '54.237.120.65']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/school'
 
